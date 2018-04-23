475ef9d798c3a101d31b71e37e072cb31ef1a9aa
Sanemat <o.gata.ken@gmail.com>
2018-04-23 21:06:40 +0700

    feat(store): update account

--- app.rb
+++ app.rb
@@ -4,7 +4,23 @@ get '/ping' do
   'pong'
 end
 
+@@store = {
+    histories: [],
+    accounts: {},
+}
+
 post '/v1/payments' do
-  puts JSON.parse(request.body.read)
+  payload = JSON.parse(request.body.read)
+  account = @@store[:accounts][payload['account_id']] || build_account
+  updated_account = { balance: account[:balance] - payload['amount'] }
+  @@store[:accounts][payload['account_id']] = updated_account
   201
+end
+
+def build_account
+  { balance: rand(positive_balance_amount_range) }
+end
+
+def positive_balance_amount_range
+  1..10000
 end
