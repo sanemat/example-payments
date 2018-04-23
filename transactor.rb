326b57093cfdd68444fb53e6e29db16a8f3d0e2a
Sanemat <o.gata.ken@gmail.com>
2018-04-23 12:59:54 +0700

    feat(transaction): add producer_id

--- transactor.rb
+++ transactor.rb
@@ -15,9 +15,10 @@ class Transactor
 
     def start(count:)
       puts 'start!'
+      producer_id = SecureRandom.uuid
 
       loop do
-        puts "POST #{api_v1_payments} #{build_transaction(account_id: SecureRandom.uuid, amount: rand(amount_range))}"
+        puts "POST #{api_v1_payments} #{build_transaction(producer_id: producer_id, account_id: SecureRandom.uuid, amount: rand(amount_range))}"
         sleep 1
       end
     end
@@ -34,8 +35,9 @@ class Transactor
       "#{@endpoint}/v1/payments"
     end
 
-    def build_transaction(account_id:, amount:)
+    def build_transaction(producer_id:, account_id:, amount:)
       {
+          producer_id: producer_id,
           account_id: account_id,
           amount: amount,
       }
